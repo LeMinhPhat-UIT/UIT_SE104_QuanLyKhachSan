5cff59de1b2a5eb493d6a6f2ccc2d832d4a60548
LeMinhPhat <23521140@gm.uit.edu.vn>
2025-07-10 21:50:43 +0700

    fix price format in SettingWindow.xaml, fix CheckOut start from CheckIn in BookingRoomWindow.xaml

--- QuanLyKhachSan/obj/Debug/net8.0-windows/QuanLyKhachSan.AssemblyInfo.cs
+++ QuanLyKhachSan/obj/Debug/net8.0-windows/QuanLyKhachSan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+942b8d2bee4808a5f73d04b1d1fab0f4bc227482")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe0195f2cd5f2092dbbc307d3e87930cf52966e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
